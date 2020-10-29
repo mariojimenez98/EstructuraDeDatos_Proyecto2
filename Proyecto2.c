068ff78fcf7c697686284f57c4e2f2a93622c571
Mario Jimenez <50642172+mariojimenez98@users.noreply.github.com>
2020-10-29 10:16:37 -0600

    Add files via upload
    Primer commit

---
+++ Proyecto2.c
@@ -0,0 +1,8 @@
+/********************************************
+    Nombre de Alumnos: Mario Ivan Jimenez Alfaro #727740
+
+    Fecha: 7 de Noviembre 2020
+    Materia: Estructura de Datos
+    Programa: Proyecto 2
+    Descripcion: Simulador Melate
+********************************************/
